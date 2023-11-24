f69090d12307ea4ac5cecb99e9c175faf4830ad9
micaelenn <mirandamicaelen@gmail.com>
2023-11-23 22:05:27 -0300

    feat: add sanity-plugin-vercel-deploy

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,7 +1,11 @@
+// external modules
 import { defineConfig } from 'sanity'
 import { deskTool } from 'sanity/desk'
 import { visionTool } from '@sanity/vision'
 import { schemaTypes } from './schemas'
+import { vercelDeployTool } from 'sanity-plugin-vercel-deploy'
+
+// internal modules
 import { projectID, dataset } from './utils/config/enviroment'
 import { structure } from './structure/index'
 
@@ -12,7 +16,11 @@ export default defineConfig({
   projectId: projectID,
   dataset: dataset,
 
-  plugins: [deskTool({structure}), visionTool()],
+  plugins: [
+    deskTool({structure}),
+    visionTool(),
+    vercelDeployTool(),
+  ],
 
   schema: {
     types: schemaTypes,
